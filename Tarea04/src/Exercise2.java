c8c3cbf23305a2b4af263edf73353cbc7c8520aa
ivandavid858 <ivandavid858@gmail.com>
2023-10-04 16:29:08 +0200

    Exercise 2 done

---
+++ Tarea04/src/Exercise2.java
@@ -0,0 +1,19 @@
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+
+		int N = 5;
+		double A = 4.56;
+		char C = 'r';
+		
+		System.out.println(N);
+		System.out.println(A);
+		System.out.println(C);
+		System.out.println(N+A);
+		System.out.println(A-N);
+		System.out.println((int)C);
+
+	}
+
+}

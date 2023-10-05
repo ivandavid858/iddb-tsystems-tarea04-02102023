ce52db87b0056dc6043a0eeaa79dedd0e7af4101
ivandavid858 <ivandavid858@gmail.com>
2023-10-05 09:59:07 +0200

    Exercise 3 done

---
+++ Tarea04/src/Exercise3.java
@@ -0,0 +1,35 @@
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+
+		int X = 2;
+		int Y = 3;
+		double M = 4.5;
+		double N = 2.3;
+		
+		System.out.println(X+Y);
+		System.out.println(X-Y);
+		System.out.println(X*Y);
+		System.out.println(X/Y);
+		System.out.println(X%Y);
+		System.out.println(N+M);
+		System.out.println(N-M);
+		System.out.println(N*M);
+		System.out.println(N/M);
+		System.out.println(N%M);
+		System.out.println(X+N);
+		System.out.println(X-N);
+		System.out.println(X*N);
+		System.out.println(X/N);
+		System.out.println(X%N);
+		System.out.println(2*X);
+		System.out.println(2*Y);
+		System.out.println(2*N);
+		System.out.println(2*M);
+		System.out.println(X+Y+N+M);
+		System.out.println(X*Y*N*M);
+		
+	}
+
+}

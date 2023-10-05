963ba370af5b09d35a128f13029317272a7fee08
ivandavid858 <ivandavid858@gmail.com>
2023-10-05 10:02:29 +0200

    Exercise 4 done

---
+++ Tarea04/src/Exercise4.java
@@ -0,0 +1,16 @@
+
+public class Exercise4 {
+
+	public static void main(String[] args) {
+
+		int N = 4;
+		int N2 = N + 77;
+		int N3 = 2*N;
+		
+		System.out.println(N);
+		System.out.println(N2);
+		System.out.println(N3);
+		
+	}
+
+}
